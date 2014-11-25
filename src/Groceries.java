296ed2fc422697a3f6d7ec994b35c726f87a8e7d
Kyle Waltz <kylewaltz@yahoo.com>
2014-11-25 08:18:05 -0700

    Update Groceries.java

--- src/Groceries.java
+++ src/Groceries.java
@@ -1,1 +1,15 @@
 // main program to run the GroceryList
+// main program to run the GroceryList
+
+public class GroceryMain
+{
+  public static void Main (String[] args)
+  {
+    GroceryList otherList = new GroceryList();
+    GroceryItem Item1 = new GroceryList("Oreos", 3, 2.50);
+    GroceryItem Item2 = new GroceryList("Goldish", 2, 1.75);
+    GroceryList Item3 = new GroceryList("Apple", 8, 0.23);
+    GroceryList Item4 = new GroceryList("Fruit Loops", 2, 4.32);
+    GroceryList Item5 = new GroceryList("PopTarts", 4, 3.67);
+  }
+}
